666af52d96b361618bbfabfd816eaa9c3e99c594
Divakar  M <draju@outlook.com>
2020-12-29 14:29:36 +0000

    Reading Keyboard Inputs

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"errors"
 	"fmt"
@@ -22,8 +23,25 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
+
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
+
 		if shouldConvertAgain != "Y" {
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
@@ -36,4 +54,8 @@ func printError(err error) {
 func convertToCelsius(value float64) {
 	convertedValue := (value - 32) * 5 / 9
 	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
+}
+func convertToFahrenheit(value float64) {
+	convertedValue := (value * 9 / 5) + 32
+	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
 }
